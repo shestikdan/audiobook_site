a5a5cb48f1136f0fed6a63e66069e1557bc3883c
shestikdan <shestopalovdanil2000@gmail.com>
2024-05-12 03:28:01 +0500

    add allowed hosts ip

--- audiobook_site/settings.py
+++ audiobook_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ypgxacf9s)sjc853q6v!l$4b#+tzp(hho0r@!vh48!8rh!hi9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['62.84.119.148', 'localhost', '127.0.0.1']
 
 
 # Application definition
